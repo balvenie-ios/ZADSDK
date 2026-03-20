2ed158e4b145abac5a71135693246d1d9bcdac31
balvenie-ios <ios@baifu-tech.net>
2026-03-20 15:55:32 +0800

    [Release] 2.0.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "1.0.4"
-let checksum = "15f8503557c7aa4e515911ff951db031c1c862f111a398a7b32af8a74547007f"
+let version = "2.0.0"
+let checksum = "03ff6baa465205887f1313a6fc7080debfdb6cb107aaacc89a8b2438bcb727f3"
 
 let package = Package(
     name: "ZADSDK",
